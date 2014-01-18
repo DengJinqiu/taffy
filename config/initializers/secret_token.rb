1994eead8a95a5351f91e5d58e5d593ca270e480
Jinqiu Deng <deng.jinqiu@gmail.com>
2014-01-17 20:59:38 -0500

    Start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Taffy::Application.config.secret_key_base = '061f0826e3510548f08570724b8ca922fca60edffe58aaecc1cb706aa7cefb37fec06e7f381ac1bae2706a76fb4dadf6a2b36f0cca1129aa193f9327823b62ac'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Taffy::Application.config.session_store :cookie_store, key: '_taffy_session'
