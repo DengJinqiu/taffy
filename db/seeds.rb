e5a942c2f4c1d3ab311ad4c4a153e9add7662f34
Jinqiu Deng <deng.jinqiu@gmail.com>
2014-01-21 15:05:43 -0500

    add

--- db/seeds.rb
+++ db/seeds.rb
@@ -56,8 +56,8 @@ Enrollment.create(program_id: @pro3.id, student_id: @stu4.id)
 
 Schedule.create(course_id: @cou1.id, start_date: '2013-09-01', end_date: '2014-03-31', start_time: 600, end_time: 700, day_of_week: 2)
 Schedule.create(course_id: @cou1.id, start_date: '2013-09-01', end_date: '2014-03-31', start_time: 600, end_time: 700, day_of_week: 4)
-Schedule.create(course_id: @cou2.id, start_date: '2013-09-01', end_date: '2014-03-31', start_time: 600, end_time: 700, day_of_week: 2)
-Schedule.create(course_id: @cou3.id, start_date: '2013-09-01', end_date: '2014-03-31', start_time: 600, end_time: 700, day_of_week: 3)
+Schedule.create(course_id: @cou2.id, start_date: '2013-09-01', end_date: '2013-10-01', start_time: 600, end_time: 700, day_of_week: 2)
+Schedule.create(course_id: @cou3.id, start_date: '2013-10-01', end_date: '2014-11-31', start_time: 600, end_time: 700, day_of_week: 3)
 Schedule.create(course_id: @cou4.id, start_date: '2013-09-01', end_date: '2014-03-31', start_time: 600, end_time: 700, day_of_week: 4)
 Schedule.create(course_id: @cou4.id, start_date: '2014-01-01', end_date: '2014-02-10', start_time: 600, end_time: 700, day_of_week: 5)
 
