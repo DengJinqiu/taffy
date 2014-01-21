43c4fd79ac83ca8c5070a13eb29e283d43165ba9
Jinqiu Deng <deng.jinqiu@gmail.com>
2014-01-21 10:26:29 -0500

    change seed

--- db/migrate/20140118033720_create_schedules.rb
+++ db/migrate/20140118033720_create_schedules.rb
@@ -6,7 +6,7 @@ class CreateSchedules < ActiveRecord::Migration
       t.date :end_date
       t.integer :start_time  # in minutes
       t.integer :end_time    # in minutes
-      t.integer :day_of_week # 1 => Monday, 7 => Sunday
+      t.integer :day_of_week # 1 => Sunday, 7 => Saturday
                              # between start_date and end_date
     end
   end
